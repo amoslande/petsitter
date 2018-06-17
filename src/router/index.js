b26851559e5ac95d5b67604218ea27c17238a7f2
amos <amoslande@live.com>
2018-06-17 23:48:57 +0300

    Added NewPetView and route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import ManagePetsView from '@/views/ManagePetsView';
+import NewPetView from '@/views/NewPetView';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'ManagePetsView',
       component: ManagePetsView,
+    },
+    {
+      path: '/new',
+      name: 'NewPetView',
+      component: NewPetView,
     },
   ],
 });
---
+++ src/views/NewPetView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2 class="pets-view-title">New Pet</h2>
+    <button type="button" class="btn btn-secondary" @click="cancel">Cancel</button>
+  </div>
+</template>
+
+<script>
+import { PetsStore } from '@/stores/PetsStore.js';
+
+export default {
+  name: 'ManagePetsView',
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    cancel() {
+      this.$router.push('/');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.pets-view-title {
+  padding: 20px 10px;
+  text-align: center;
+}
+</style>
